9fe95b0d768376ccaa17e248443cd96896b6f806
Anthony Leroy <anthony.leroy@ulb.ac.be>
2023-11-29 18:34:35 +0100

    fixed container name for git repo

--- inspect_plnmonitor_db_as_grafana.sh
+++ inspect_plnmonitor_db_as_grafana.sh
@@ -1,2 +1,2 @@
 echo "Connect to db with psql as grafana user (e.g. select * from plnmonitor.lockss_box;)"
-docker exec -tiu postgres plnmonitor-grafana_plnmonitordb_1  psql -d plnmonitor -U grafana
+docker exec -tiu postgres safepln-lockssdashboard-plnmonitordb-1  psql -d plnmonitor -U grafana
